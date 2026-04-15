2a3db3a1483d1f431af5c4369e52bee0c8af565a
kirilinsky <kirill.ilinsky@gmail.com>
2026-04-15 22:54:18 +0200

    feat: update hook

--- src/components/RafflePick/RafflePick.tsx
+++ src/components/RafflePick/RafflePick.tsx
@@ -4,13 +4,13 @@ import type { RafflePickProps } from '../../types'
 export function RafflePick(_props: RafflePickProps) {
   const { buttonLabel, min, max, interval = 100 } = _props
   if (!min || !max) return
-  const { start, stop, currentValue } = useNumberCycle(min, max, interval)
+  const { start, stop, currentValue, started } = useNumberCycle(min, max, interval)
 
   return (
-    <div>
-      {currentValue} - currentValue
-      {buttonLabel && <button onClick={start}>{buttonLabel}</button>}
-      <button onClick={stop}>stop</button>
+    <div style={{ display: 'flex', gap: '5px' }}>
+      <span style={{ width: max.toString().length + 'ch' }}> {currentValue}</span>
+
+      {buttonLabel && <button onClick={started ? stop : start}>{buttonLabel}</button>}
     </div>
   )
 }
--- src/hooks/useNumberCycle.ts
+++ src/hooks/useNumberCycle.ts
@@ -4,26 +4,23 @@ import { getRandom } from '../utils/get-random'
 export const useNumberCycle = (min: number, max: number, interval: number) => {
   const [currentValue, setCurrentValue] = useState(min)
   const [startFlag, setStartFlag] = useState<boolean>(false)
-  const [stopFlag, setStopFlag] = useState<boolean>(true)
 
   const start = () => {
-    setStopFlag(false)
     setStartFlag(true)
   }
 
   const stop = () => {
-    setStopFlag(true)
     setStartFlag(false)
   }
 
   useEffect(() => {
-    if (stopFlag && !startFlag) return
+    if (!startFlag) return
     const intervalId = setInterval(() => {
       const rNum = getRandom(min, max)
       setCurrentValue(rNum)
     }, interval)
     return () => clearInterval(intervalId)
-  }, [startFlag, stopFlag])
+  }, [startFlag])
 
-  return { currentValue, start, stop }
+  return { currentValue, start, stop, started: startFlag }
 }
