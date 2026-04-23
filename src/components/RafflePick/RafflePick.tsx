8301afc865b3ad334f6b7ab92a71f0d2d0ddd1fe
kirilinsky <kirill.ilinsky@gmail.com>
2026-04-23 15:41:35 +0200

    feat: remove test calendar

--- src/components/RafflePick/RafflePick.tsx
+++ src/components/RafflePick/RafflePick.tsx
@@ -1,18 +1,19 @@
-import { Calendar } from 'react-calendar-datetime'
 import { useNumberCycle } from '../../hooks/useNumberCycle'
 import type { RafflePickProps } from '../../types'
-import 'react-calendar-datetime/style.css'
 
-export function RafflePick(_props: RafflePickProps) {
-  const { buttonLabel, min, max, interval = 100 } = _props
-  if (!min || !max) return
+export function RafflePick({
+  min = 1,
+  max = 100,
+  interval = 80,
+  buttonLabel = 'Pick Winner',
+  className,
+}: RafflePickProps) {
   const { start, stop, currentValue, started } = useNumberCycle(min, max, interval)
 
   return (
-    <div style={{ display: 'flex', gap: '5px' }}>
-      <span style={{ width: max.toString().length + 'ch' }}> {currentValue}</span>
-      <Calendar time={false}/>
-      {buttonLabel && <button onClick={started ? stop : start}>{buttonLabel}</button>}
+    <div className={className} style={{ display: 'flex', gap: '5px' }}>
+      <span style={{ width: max.toString().length + 'ch' }}>{currentValue}</span>
+      <button onClick={started ? stop : start}>{buttonLabel}</button>
     </div>
   )
 }
